5fb8c022ea470de4c6d9530ba2862ce95cc3b3bb
Gregg <gregg.hoshovsky@centurylink.net>
2018-04-11 12:22:33 -0700

    update

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -1,5 +1,10 @@
 // Dom7
 var $$ = Dom7;
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    console.log("ready");
+    console.log(Media);
+}
 
 // Framework7 App main instance
 var app  = new Framework7({
@@ -98,3 +103,77 @@ function executeAjax(urlIn,dataIn)
 
 }
 */
+
+// Audio player
+        //
+        var my_media = null;
+        var mediaTimer = null;
+
+        // Play audio
+        //
+        function playAudio(src) {
+            // Create Media object from src
+            my_media = new Media(src, onSuccess, onError);
+
+            // Play audio
+            my_media.play();
+
+            // Update my_media position every second
+            if (mediaTimer == null) {
+                mediaTimer = setInterval(function() {
+                    // get my_media position
+                    my_media.getCurrentPosition(
+                        // success callback
+                        function(position) {
+                            if (position > -1) {
+                                setAudioPosition((position) + " sec");
+                            }
+                        },
+                        // error callback
+                        function(e) {
+                            console.log("Error getting pos=" + e);
+                            setAudioPosition("Error: " + e);
+                        }
+                    );
+                }, 1000);
+            }
+        }
+
+        // Set audio volume
+        //
+        function setVolume(volume) {
+            if (my_media) {
+                my_media.setVolume(volume);
+            }
+        }
+
+        // Stop audio
+        //
+        function stopAudio() {
+            if (my_media) {
+                my_media.stop();
+            }
+            clearInterval(mediaTimer);
+            mediaTimer = null;
+        }
+
+        // onSuccess Callback
+        //
+
+        //
+         function onSuccess() {
+             console.log("playAudio():Audio Success");
+         }
+
+         // onError Callback
+         //
+         function onError(error) {
+             alert('code: '    + error.code    + '\n' +
+                   'message: ' + error.message + '\n');
+         }
+
+         // Set audio position
+         //
+         function setAudioPosition(position) {
+             document.getElementById('audio_position').innerHTML = position;
+         }
